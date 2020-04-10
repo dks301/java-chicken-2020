b837ab241c561e902b6a68a950688e693e6e8578
hj <a301dks@naver.com>
2020-04-10 16:01:53 +0900

    test: MenuCount 테스트 추가

--- src/test/java/domain/MenuCountTest.java
+++ src/test/java/domain/MenuCountTest.java
@@ -11,11 +11,24 @@ import org.junit.jupiter.api.Test;
  *    @author AnHyungJu
  */
 public class MenuCountTest {
-	@DisplayName("자연수가 아닌 메뉴 수량일 때 예외처리 확인")
+	@DisplayName("정상적으로 생성되는지 확인")
 	@Test
 	void constructorTest() {
+		assertThat(new MenuCount(3)).isInstanceOf(MenuCount.class)
+			.isNotNull();
+	}
+
+	@DisplayName("자연수가 아닌 메뉴 수량일 때 예외처리 확인")
+	@Test
+	void constructorTest2() {
 		assertThatThrownBy(() -> new MenuCount(0))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessageContaining("최소");
+	}
+
+	@DisplayName("정상적으로 메뉴 수량을 반환하는지 확인")
+	@Test
+	void getMenuCountTest() {
+		assertThat(new MenuCount(3).getMenuCount()).isEqualTo(3);
 	}
 }
